e184a04baa8d6413ce0b07b143bfa352d398c72d
João <joaopaulokimi@gmail.com>
2024-12-29 12:00:09 -0300

    projeto imóveis sem http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterModule],
+  template: `
+    <main>
+
+      <header class="brand-name">
+
+      <a [routerLink]="['/']">
+        <img class="brand-logo" src="images/logo.svg" alt="logo" aria-hidden="true"/>
+      </a>
+      </header>
+
+      <section class="content">
+
+        <router-outlet></router-outlet>
+
+      </section>
+
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'homes-app';
+  title = 'homes';
 }
